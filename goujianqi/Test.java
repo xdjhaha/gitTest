72b8a11cdbb69b69242686a0fe3a5b2d076a0e36
xdj <xudianjunxdj@163.com>
2018-03-25 22:44:03 +0800

    构建器

---
+++ goujianqi/Test.java
@@ -0,0 +1,15 @@
+package goujianqi;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bean1 bean1 = new Bean1.build("1", "2").c("c").d("d").build1();
+
+		System.out.println(bean1);
+	}
+
+}
